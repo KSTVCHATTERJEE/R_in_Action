f9e8bbca12b14c0e2114da51b8a2dff0b0322830
Kaustav Chatterjee <kstvchatterjee@gmail.com>
2017-08-18 12:58:49 +0530

    18th August 2017

---
+++ Chap_01.R
@@ -0,0 +1,23 @@
+demo(graphics)
+
+demo(graphics)
+
+demo(Hershey)
+getwd()
+q()
+ls()
+options()
+options(digits=2)
+options()
+x <- runif(20)
+summary(x)
+hist(x)
+savehistory()
+x
+options(digits=2)
+x <- runif(20)
+x
+summary(x)
+history(4)
+library()
+search()
