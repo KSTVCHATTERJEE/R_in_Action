b4bc21bd80081089c634dded20ac217311688096
Kaustav Chatterjee <kstvchatterjee@gmail.com>
2017-08-24 21:01:28 +0530

    24th August 2017

--- R_Assignment_1.R
+++ R_Assignment_1.R
@@ -3,12 +3,14 @@ marks_Q1 <- rep(c(10,20,30,10,40,50),c(3,5,2,4,3,4))
 marks_Q1
 table(marks_Q1)
 cbind(table(marks_Q1))
+pie(table(marks_Q1))
 
 #Q2_part1
 marks_Q2 <- c(10,12,13,14,17)
 marks_Q2
 mean_Q2_1 <- mean(marks_Q2)
 mean_Q2_1
+barplot(marks_Q2)
 
 #Q2_part2
 no_of_students <- c(5,7,9,8,4)
@@ -36,3 +38,15 @@ mean_Q2_3
 
 #Q3
 
+marks_Q3 <- c(12,63,61,63,87,65,1,24,67,52,70,55,29,93,30,84,29,27,19,
+           24,32,31,16,4,22,82,53,92,84,5,46,27,31,51,19,76,21,26,
+           100,81,56,65,32,63,33,51,68,49,25,77)
+marks_Q3
+range(marks_Q3)
+breaks <- seq(0,100,by=10)
+breaks
+marks_Q3.cut <- cut(marks_Q3,breaks)
+marks_Q3.cut
+marks_Q3.table <- table(marks_Q3.cut)
+marks_Q3.table
+cbind(marks_Q3.table)
