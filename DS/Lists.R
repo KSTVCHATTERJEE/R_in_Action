4444dc0f57833acd1790f500b8f50d75e8ec0650
Kaustav Chatterjee <kstvchatterjee@gmail.com>
2017-08-25 19:47:01 +0530

    25th August 2017

--- DS/Lists.R
+++ DS/Lists.R
@@ -1,27 +1,32 @@
-s <- c("My First List")
+s <- c("My First List")#a string
 s
-v1 <- c(25,26,18,39)
+v1 <- c(25,26,18,39)#a numeric vector
 v1
-m1 <- matrix(1:10,nrow=2,byrow=T)
+m1 <- matrix(1:10,nrow=2,byrow=T)#a numeric matrix
 m1
-vos <- c("one","two","three")
+vos <- c("one","two","three")#a string vector
 vos
+#data frame building starts
 formno <- c(17010,17045,17012)
 name <- c("Achal","Apoorva","Goldie")
 gender <- c('M','F','M')
 formno;name;gender
 stu_records <- data.frame(formno,name,gender,stringsAsFactors=F)
 stu_records
+#data frame building ends
+#stringsAsFactors makes sure strings are not stored as factors
 str(stu_records)
-mylist=list(s,v1,m1,vos,stu_records)
+mylist=list(s,v1,m1,vos,stu_records)#list gets created
 mylist
-str(mylist)
+str(mylist)#structure of mylist
 mylist <- list(title=s,ages=v1,m1,vos,students=stu_records)
-mylist
-mylist[2]
-mylist[3]
-mylist[['ages']]
-mylist$ages
-sum(mylist[[3]])
-mylist$students$name#names of students from the data frame in the list
-mylist[[2]]
+#s, v1 and stu_records gets renamed as title, ages and students respectively 
+mylist#changes shown
+mylist[2]#2nd object of list 
+mylist[3]#3rd object of list
+mylist[['ages']]#2nd object of list (renamed as 'ages')
+mylist$ages#prints 'ages' object of mylist
+sum(mylist[[3]])#prints sum of elements of 3rd object of the list
+mylist$students$name
+#prints 'name' column of 'students' object (previously stu_records) of list
+mylist[[2]]#2nd object of list
