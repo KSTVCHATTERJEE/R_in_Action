98d97e43b558740a408ac6759817ea17f3633060
Kaustav Chatterjee <kstvchatterjee@gmail.com>
2017-09-01 12:21:45 +0530

    1st September 2017

---
+++ File/file1.R
@@ -0,0 +1,20 @@
+#Input data from keyboard
+
+x = scan()
+x
+mean(x)
+y=scan() #error for character copying
+?scan
+y = scan(what='character')
+y
+z = scan()
+z
+median(z)
+
+x=c(1:12,1,5,5)
+x
+table(x)
+names(table(x))
+names1=names(sort(table(x),decr=T))
+mode=names1[1]
+mode
