77c25f3f30b7f49b123cd96f9fa9b17accf61ffe
Kaustav Chatterjee <kstvchatterjee@gmail.com>
2017-11-15 12:06:59 +0530

    15th November 2017

---
+++ Stats/anova.R
@@ -0,0 +1,10 @@
+sup1 = c(18.5,24,17.2,19.9,18)
+sup2 = c(26.3,25.3,24,21.2,24.5)
+sup3 = c(20.6,25.2,20.8,24.7,22.9)
+sup4 = c(25.4,19.9,22.6,17.5,20.4)
+df=data.frame(sup1,sup2,sup3,sup4)
+df
+colMeans(df)
+round((addmargins(as.matrix(df),c(1,2),FUN=list(list(mean,sd),list(mean)))),1)
+stack(df)
+unstack(stack(df))
