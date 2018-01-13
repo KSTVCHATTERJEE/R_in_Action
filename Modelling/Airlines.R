0eef8ec1e6c893c4077f39b53437ca706703e8df
Kaustav Chatterjee <31096402+KSTVCHATTERJEE@users.noreply.github.com>
2018-01-13 17:45:51 +0530

    13th January 2018

---
+++ Modelling/Airlines.R
@@ -0,0 +1,31 @@
+data <- read.csv(file.choose())
+str(data)
+summary(data)
+colSums(is.na(data))
+
+library(caret)
+preproc <- preProcess(data)
+airlinesNorm <- predict(preproc,data)
+summary(airlinesNorm)
+
+#Hierarchical Clustering
+distan <- dist(airlinesNorm, method = "euclidean")
+ClusterAirline <- hclust(distan, method="ward.D")
+plot(ClusterAirline)
+
+AirlineCluster <- cutree(ClusterAirline,k=5)
+table(AirlineCluster)
+
+MeanComp <- function(var,clustergrp,meas){
+  z<-tapply(var,clustergrp,meas)
+  print(z)
+}
+
+Bal_mean <- MeanComp(data$Balance, AirlineCluster, mean)
+
+#k-means clustering
+set.seed(88)
+k <- 5
+AirlineCluster_k <- kmeans(airlinesNorm, centers=k, iter.max = 1000)
+table(AirlineCluster_k$cluster)
+AirlineCluster_k$centers
