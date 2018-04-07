4425e6ee061f4d9b977495f183d61f205eb5f8e2
Kaustav Chatterjee <kstvchatterjee@gmail.com>
2018-04-07 16:23:02 +0530

    7th April 2018

---
+++ Vidooly/Advertisements.R
@@ -0,0 +1,83 @@
+ad_org_train=read.csv("F:/R_in_Action/Vidooly/ad_org_train.csv",stringsAsFactors = FALSE)
+ad_org_test=read.csv("F:/R_in_Action/Vidooly/ad_org_test.csv",stringsAsFactors = FALSE)
+str(ad_org_train)
+str(ad_org_test)
+summary(ad_org_train)
+colSums(is.na(ad_org_train))
+fix(ad_org_train)
+
+for (i in 1:14999){
+  for(j in 2:6){
+  if(ad_org_train[i,j]=='F'){
+    ad_org_train[i,j]=NA;
+}
+}
+}
+
+for (k in 3:6){
+  ad_org_train[,k]=as.integer(ad_org_train[,k])
+}
+
+ad_org_train$views[is.na(ad_org_train$views)]=median(ad_org_train$views,na.rm=T)
+ad_org_train$likes[is.na(ad_org_train$likes)]=median(ad_org_train$likes,na.rm=T)
+ad_org_train$dislikes[is.na(ad_org_train$dislikes)]=median(ad_org_train$dislikes,na.rm=T)
+ad_org_train$comment[is.na(ad_org_train$comment)]=median(ad_org_train$comment,na.rm=T)
+
+for (i in 1:nrow(ad_org_train))
+{
+if((grepl('H',ad_org_train$duration[i])==TRUE) & (grepl('M',ad_org_train$duration[i])==TRUE) & (grepl('S',ad_org_train$duration[i])==TRUE))
+{
+ad_org_train$hour[i]=substr(ad_org_train$duration[i],regexpr('T',ad_org_train$duration[i])+1,regexpr('H',ad_org_train$duration[i])-1);
+ad_org_train$min[i]=substr(ad_org_train$duration[i],regexpr('H',ad_org_train$duration[i])+1,regexpr('M',ad_org_train$duration[i])-1);
+ad_org_train$sec[i]=substr(ad_org_train$duration[i],regexpr('M',ad_org_train$duration[i])+1,regexpr('S',ad_org_train$duration[i])-1);
+}
+      
+else if((grepl('H',ad_org_train$duration[i])==FALSE) & (grepl('M',ad_org_train$duration[i])==TRUE) & (grepl('S',ad_org_train$duration[i])==TRUE))
+{
+ad_org_train$hour[i]=0;
+ad_org_train$min[i]=substr(ad_org_train$duration[i],regexpr('T',ad_org_train$duration[i])+1,regexpr('M',ad_org_train$duration[i])-1);
+ad_org_train$sec[i]=substr(ad_org_train$duration[i],regexpr('M',ad_org_train$duration[i])+1,regexpr('S',ad_org_train$duration[i])-1);
+}
+else if((grepl('H',ad_org_train$duration[i])==FALSE) & (grepl('M',ad_org_train$duration[i])==FALSE) & (grepl('S',ad_org_train$duration[i])==TRUE))
+{
+ad_org_train$hour[i]=0;
+ad_org_train$min[i]=0;
+ad_org_train$sec[i]=substr(ad_org_train$duration[i],regexpr('T',ad_org_train$duration[i])+1,regexpr('S',ad_org_train$duration[i])-1);
+}
+else if((grepl('H',ad_org_train$duration[i])==TRUE) & (grepl('M',ad_org_train$duration[i])==TRUE) & (grepl('S',ad_org_train$duration[i])==FALSE))
+{
+ad_org_train$hour[i]=substr(ad_org_train$duration[i],regexpr('T',ad_org_train$duration[i])+1,regexpr('H',ad_org_train$duration[i])-1);
+ad_org_train$min[i]=substr(ad_org_train$duration[i],regexpr('H',ad_org_train$duration[i])+1,regexpr('M',ad_org_train$duration[i])-1);
+ad_org_train$sec[i]=0;
+}
+else if((grepl('H',ad_org_train$duration[i])==TRUE) & (grepl('M',ad_org_train$duration[i])==FALSE) & (grepl('S',ad_org_train$duration[i])==FALSE))
+{
+ad_org_train$hour[i]=substr(ad_org_train$duration[i],regexpr('T',ad_org_train$duration[i])+1,regexpr('H',ad_org_train$duration[i])-1);
+ad_org_train$min[i]=0;
+ad_org_train$sec[i]=0;
+}
+else if((grepl('H',ad_org_train$duration[i])==FALSE) & (grepl('M',ad_org_train$duration[i])==TRUE) & (grepl('S',ad_org_train$duration[i])==FALSE))
+{
+ad_org_train$hour[i]=0;
+ad_org_train$min[i]=substr(ad_org_train$duration[i],regexpr('T',ad_org_train$duration[i])+1,regexpr('M',ad_org_train$duration[i])-1);
+ad_org_train$sec[i]=0;
+}
+else if((grepl('H',ad_org_train$duration[i])==TRUE) & (grepl('M',ad_org_train$duration[i])==FALSE) & (grepl('S',ad_org_train$duration[i])==TRUE))
+{
+ad_org_train$hour[i]=substr(ad_org_train$duration[i],regexpr('T',ad_org_train$duration[i])+1,regexpr('H',ad_org_train$duration[i])-1);
+ad_org_train$min[i]=0;
+ad_org_train$sec[i]=substr(ad_org_train$duration[i],regexpr('H',ad_org_train$duration[i])+1,regexpr('S',ad_org_train$duration[i])-1);
+}
+}
+head(ad_org_train)
+
+ad_org_train$durationnew=(ad_org_train$hour*3600)+(ad_org_train$min*60)+(ad_org_train$sec)
+str(ad_org_train)
+
+for (j in 10:12){
+  ad_org_train[,j]=as.integer(ad_org_train[,j])
+}
+ad_org_train$durationnew
+
+cor(ad_org_train[,c(2,3,4,5,6,13)])
+
